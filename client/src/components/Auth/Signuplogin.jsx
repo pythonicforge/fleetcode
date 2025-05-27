e70d5a5202f5f6c9be2dd15426a2cb97e988b513
ansh <ansh@Anshs-MacBook-Pro-4.local>
2025-05-27 11:49:16 +0530

    created the folder structure

--- client/src/components/Auth/Signuplogin.jsx
+++ client/src/components/Auth/Signuplogin.jsx
@@ -1,4 +1,4 @@
-import supabase from "./client/supabaseclient";
+import supabase from "../client/supabaseclient";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 
